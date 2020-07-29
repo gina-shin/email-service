5a52376532b11c95d0ee42ed94ef9ee45a7c902b
Gina Shin <gina.shin@colorado.edu>
2020-07-29 17:17:17 -0600

    Extrapolating authorization as a middleware.

--- index.js
+++ index.js
@@ -5,20 +5,10 @@ const bodyParser = require('body-parser')
 
 const { config } = require('./config')
 const registerRoutes = require('./routes/emailRoutes')
+const { authorize } = require('./middlewares/authorization')
 
 app.use(bodyParser.json())
-app.use((req, res, next) => {
-  if(req.headers.authorization !== config.secretKey) {
-    res.status(403)
-    .json({
-      status: 403,
-      message: 'Invalid credentials.'
-    })
-  }
-  else {
-    next()
-  }
-})
+app.use(authorize(config.secretKey))
 app.use('/', registerRoutes.emailRoutes(express.Router(), config))
 
 app.listen(port, () => console.log(`Email service listening at http://localhost:${port}`))
---
+++ middlewares/authorization/index.js
@@ -0,0 +1,14 @@
+const authorize = (secretKey) => (req, res, next) => {
+  if(req.headers.authorization !== secretKey) {
+    res.status(403)
+    .json({
+      status: 403,
+      message: 'Invalid credentials.'
+    })
+  }
+  else {
+    next()
+  }
+}
+
+module.exports = { authorize }
